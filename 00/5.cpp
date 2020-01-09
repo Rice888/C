344aafac7f7a01278b2a1d257f3c8fa4dc3a8b6a
Rice <rice88@outlook.com>
2020-01-09 17:04:53 +0800

    逻辑判断

---
+++ 00/5.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int number, total;
+    printf("Please enter the number of customers:\n");
+    scanf("%d", &number);
+    total = 300 * number;
+    if (total >= 3000)
+    {
+        total = total * 0.8;
+    }
+
+    printf("Total %d\n", total);
+
+    return 0;
+}
